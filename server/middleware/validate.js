0940a4c5ff4815bae4f2f5906062d77800219b99
Deepanshu Sharma <sharmadeepanshu1005@gmail.com>
2026-02-15 10:17:07 +0530

    add request validation middleware

---
+++ server/middleware/validate.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+const AppError = require('../utils/AppError');
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const messages = errors.array().map((err) => err.msg);
+    throw AppError.badRequest(messages.join(', '));
+  }
+
+  next();
+};
+
+module.exports = validate;
